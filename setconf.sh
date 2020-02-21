bedfbae610a41e17df116cdc27a16c4f115a13d8
genesem <genesem@gmail.com>
2020-02-21 15:10:48 +0500

    updates

--- setconf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Set nginx resolver.."
-conf="resolver $(/usr/bin/awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf); resolver_timeout 2s;" 
-echo "Got: $conf" 
-echo "$conf" >/etc/nginx/common.conf
-nginx -s reload >/dev/null
-exit 0
-
-
